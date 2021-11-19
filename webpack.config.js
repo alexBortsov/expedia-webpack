ec2f2afa12524d7b9aa7d30a7c37a5ec1ad8a520
AlexanderBortsov <bortsov78saltek@gmail.com>
2021-11-19 23:33:33 +0200

    save commit 23:33

--- webpack.config.js
+++ webpack.config.js
@@ -11,52 +11,37 @@ module.exports = {
       filename: '[name].[contenthash].js',
       assetModuleFilename: "assets/[hash][ext][query]",
       clean: true,
-  },
+   },
    devtool: 'source-map',
    plugins: [
       new MiniCssExtractPlugin({
          filename: '[name].[contenthash].css'
       }),
       new HtmlWebpackPlugin({
-      template: "./src/index.html"
-   })],
+         template: "./src/index.html"
+      })],
    module: {
       rules: [
          {
             test: /\.html$/i,
             loader: "html-loader",
-            },
+         },
          {
             test: /\.(sa|sc|c)ss$/,
             use: [
                (mode === 'development') ? "style-loader" : MiniCssExtractPlugin.loader,
                "css-loader",
-               {
-                  loader: "postcss-loader",
-                  options: {
-                      postcssOptions: {
-                          plugins: [
-                              [
-                                  "postcss-preset-env",
-                                  {
-                                      // Options
-                                  },
-                              ],
-                          ],
-                      },
-                  },
-              },
-              "sass-loader",
+               "sass-loader",
             ],
          },
          {
             test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
+            type: 'asset/resource',
          },
          {
             test: /\.(woff|woff2|eot|ttf|otf)$/i,
             type: 'asset/resource',
-        },
+         },
       ]
    },
 }
