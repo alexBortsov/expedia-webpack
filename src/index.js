04a9480424cd9783ac225eceece2e1a19a776da7
alexBortsov <bortsov78saltek@gmail.com>
2021-11-20 07:20:18 +0200

    save 7-24

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 import './styles/index.scss';
-//import $ from "jquery";
+import $ from "jquery";
+import '../node_modules/slick-carousel/slick/slick';
+import '../node_modules/slick-carousel/slick/slick-theme.scss';
+import '../node_modules/slick-carousel/slick/slick.scss';
 
+
+$(document).ready(function(){
+   $('.burger-menu').click(function (event) {
+       $('.burger-menu, .header__menu').toggleClass('active');
+   }); 
+});
+$('.container-slider').slick({
+   infinite: true,
+   slidesToShow: 3,
+   slidesToScroll: 3,
+   autoplay: true,
+   autoplaySpeed: 2000
+});
 const userStack = {
    language: 'JavaScript',
    framework: 'Angular'
